014eafddc7fc34d3d53dbcae51a7363f19e6a79e
Andrzej Parszuto <andrzej@Andrzejs-MacBook-Pro.local>
2015-02-10 07:44:45 +0100

    Initial Commit

---
+++ U Network Simulator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  U Network Simulator
+//
+//  Created by Andrzej Parszuto on 2/10/15.
+//  Copyright (c) 2015 U. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
