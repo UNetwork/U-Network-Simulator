5b01d1c435387705d0f3c413daf83229b975f3b4
Andrzej Parszuto <a@u-is-not-inter.net>
2015-03-03 07:21:03 +0100

    1stRead added

---
+++ 1stRead.swift
@@ -0,0 +1,43 @@
+/*
+
+First thing needed to be said is that U is not Internet.
+
+U is a user owned, self sustainable, secure digital services network.
+
+U is using P2P architecture, every user has the same rights and properties.
+
+U is free and open to anyone without any agreements, charges or limitations, as long as you are a part of it, and your software and hardware are doing the basic stuff right - transferring your and other users’ data over the network.
+
+U network is using a new routing and transfer algorithm, which enables global network operation without any kind of hierarchical structure, control nodes, central servers nor authorities.
+
+The network runs on free and open source software.
+
+The services covered by U network software include:
+Messaging,
+Data storage and sharing,
+Publishing,
+Digital wallet for digital currencies,
+Search,
+Social network,
+Distributed data backup,
+API for any kind of future applications that can be designed and distributed independently by anyone.
+
+All information and data that you enter to the network is stored on your own hardware, running open source software designed to keep your data private and manageable. This gives back to you full control over your digital data and communication. No license agreements for any kind of services. They are simply not possible, because there's no owner nor operator of the U network. It’s up to you: either use U network, or stay with corporate and state managed networks and services.
+
+U network consists of three families of devices:
+U Home Stations,
+Self sustainable U network routers,
+U Screens - network access applications that can be installed on smart phone, tablet, PC etc.
+
+The most important and most powerful element of the U network is the U Home Station. If you want to move to U network, you need one of these devices. This device will store your identity, your data, and all the stuff that you want to keep secure but shareable and manageable.
+All you need is to connect this device to power source, choose your name and the way to connect to the U network.
+U Home Station has built-in interfaces that allow to connect it to other nodes via wireless, wired or wide area network. Once you connect to another node, the setup is done.
+
+Extending network range of Home Stations is possible due to U routers. They just receive and transfer network data. Each device in this family may have an independent power source, such as  solar battery or micro wind generator. U routers have also chargeable batteries which allow them to run for a considerable amount of time without the primary power source. Devices are weather secured, so all you need to use them is to put them in a place with a bit of electricity, sun or wind. Done, U network has grown.
+
+Third family are not devices at all. This is the software you can install on already owned devices to gain access to the U network and your U Home Station.
+
+to be continued…
+
+*/
+
