1364889abcf865a697a46fa4266123bac070af8a
Andrzej Parszuto <a@u-is-not-inter.net>
2015-06-08 08:48:17 +0200

    test 2
    test2

---
+++ U Network Simulator/URouterSmart.swift
@@ -0,0 +1,14 @@
+//
+//  URouterSmart.swift
+//  U Network Simulator
+//
+//  Created by Andrzej Parszuto on 08/06/15.
+//  Copyright (c) 2015 U. All rights reserved.
+//
+
+import Foundation
+
+class URouterSmart: URouterSimpleDirection
+{
+    
+}
